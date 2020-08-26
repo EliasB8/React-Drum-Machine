a9b92f632d205eb83b1256b0ad6be2de1fcb7b7f
Elias Ferede <47753722+EliasB8@users.noreply.github.com>
2020-08-25 19:58:21 -0700

    Set defaults

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,9 +1,34 @@
 import React from "react";
+import ToggleButton from "./ToggleButton";
 
-function Controller() {
+function Controller(props) {
   return (
     <div className="controller-container">
-      Controllers will go here for sure
+      <ToggleButton
+        name="power"
+        checked={false}
+        label="Power"
+        state1="on"
+        state2="off"
+      />
+      <div id="bank-display" className="bank-display"></div>
+      <div class="slidecontainer">
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={props.volume}
+          class="slider"
+          id="myRange"
+        />
+      </div>
+      <ToggleButton
+        name="bank"
+        checked={true}
+        label="Bank"
+        state1="HT"
+        state2="SPK"
+      />
     </div>
   );
 }
